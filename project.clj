56ce1dbb6a08e96150dc85934caecfeb68108a53
Amith George <amith@Amiths-MacBook-Pro.local>
2015-05-14 12:48:07 +0530

    solution for 214 intermediate

--- project.clj
+++ project.clj
@@ -3,4 +3,6 @@
   :url "https://www.reddit.com/r/dailyprogrammer/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]]
+  ;:main rdp.214-intermediate
+  )
