44eb22cf05732d622f9fbe82f977ea018a18a054
Amith George <amith@Amiths-MacBook-Pro.local>
2015-05-15 12:15:50 +0530

    removed plugin no.disassemble

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0-beta3"]]
-  :plugins [[lein-nodisassemble "0.1.3"]]
+  ;; :plugins [[lein-nodisassemble "0.1.3"]]
   :jvm-opts ^:replace ["-Xms1024m" "-Xmx1g" "-server"]
   :main rdp.214-intermediate
   )
